3211110019ab8715016a82694db1102501530c1e
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-11-26 14:16:29 +0100

    Initial commit

---
+++ src/main/java/org/example/backendwakandaagua/BackendWakandaAguaApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandaagua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWakandaAguaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWakandaAguaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/backendwakandaagua/BackendWakandaAguaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandaagua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWakandaAguaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
